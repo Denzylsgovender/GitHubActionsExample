a94a7cac974fcce7ca0b909faefb77f433e9ad71
Denzyl Soobramoney Govender <135228226+Denzylsgovender@users.noreply.github.com>
2025-05-15 21:31:39 +0200

    Add files via upload

---
+++ GitHubWorkflow/src/main/java/com/mycompany/githubworkflow/GitHubWorkflow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubworkflow;
+
+/**
+ *
+ * @author dengovender
+ */
+public class GitHubWorkflow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
